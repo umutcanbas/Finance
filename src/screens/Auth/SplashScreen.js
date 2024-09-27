c299d9f27b6df9d4bad153cd3c1cdb358a835b9d
umutcanbas <umutbas57@gmail.com>
2024-09-27 15:25:25 +0300

    Splash screen page

--- src/screens/Auth/SplashScreen.js
+++ src/screens/Auth/SplashScreen.js
@@ -1,14 +1,40 @@
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 
-const SplashScreen = () => {
+import routes from '../../navigation/routes';
+
+import {useSelector} from 'react-redux';
+
+const SplashScreen = ({navigation}) => {
+  const {isLogged} = useSelector(state => state.user);
+
+  useEffect(() => {
+    if (isLogged) {
+      navigation.replace(routes.APP_NAVIGATOR);
+    } else {
+      navigation.replace(routes.LOGIN);
+    }
+  }, []);
+
   return (
-    <SafeAreaView>
-      <Text>SplashScreen</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Finance</Text>
     </SafeAreaView>
   );
 };
 
 export default SplashScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#6495ed',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 35,
+    color: 'white',
+  },
+});
