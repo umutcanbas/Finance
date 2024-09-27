6c6edfe25247d3de8b178f43c5a8abdb86e0c4a1
umutcanbas <umutbas57@gmail.com>
2024-09-27 15:24:28 +0300

    Button component

---
+++ src/components/Button.js
@@ -0,0 +1,35 @@
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+
+const CustomButton = ({title, onPress}) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={styles.buttonText}>{title}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default CustomButton;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#f8f8ff',
+    width: 120,
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 45,
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
