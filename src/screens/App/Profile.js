6e24607ff5c570410886ae1d0053a136c0f7e050
umutcanbas <umutbas57@gmail.com>
2024-10-04 14:39:03 +0300

    Pie card logic

--- src/screens/App/Profile.js
+++ src/screens/App/Profile.js
@@ -1,33 +1,102 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import CustomButton from '../../components/Button';
+import React, {useMemo} from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+
+import {useDispatch, useSelector} from 'react-redux';
 
 import {logOut} from '../../redux/user';
-import {useDispatch} from 'react-redux';
 import routes from '../../navigation/routes';
+
+import PieCard from '../../components/PieCard';
+import LogOutLogo from '../../assets/svg/logout-box.svg';
 
 const Profile = ({navigation}) => {
   const dispatch = useDispatch();
+  const moneyStatus = useSelector(state => state.user.moneyStatus);
+
+  const expenseData = useMemo(() => {
+    return moneyStatus
+      .filter(item => item.type === 'expense')
+      .reduce((acc, item) => {
+        const category = item.category;
+        const money = parseFloat(item.money);
+
+        if (!acc[category]) {
+          acc[category] = 0;
+        }
+        acc[category] += money;
+
+        return acc;
+      }, {});
+  }, [moneyStatus]);
+
+  const incomeData = useMemo(() => {
+    return moneyStatus
+      .filter(item => item.type === 'income')
+      .reduce((acc, item) => {
+        const category = item.category;
+        const money = parseFloat(item.money);
+
+        if (!acc[category]) {
+          acc[category] = 0;
+        }
+        acc[category] += money;
+
+        return acc;
+      }, {});
+  }, [moneyStatus]);
 
   const handleLogOut = () => {
     dispatch(logOut());
-
     navigation.replace(routes.AUTH_NAVIGATOR, {screen: routes.LOGIN});
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Profile</Text>
-      <CustomButton title="LogOut" onPress={handleLogOut} />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>Profile</Text>
+        <TouchableOpacity style={styles.button} onPress={handleLogOut}>
+          <LogOutLogo width={30} height={30} />
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView style={styles.innerContainer}>
+        <PieCard data={expenseData} title="Expense" />
+        <PieCard data={incomeData} title="Income" />
+      </ScrollView>
+    </View>
   );
 };
 
 export default Profile;
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:'#6495ed'
+  container: {
+    flex: 1,
+    backgroundColor: '#6495ed',
+    paddingVertical: 50,
+    paddingHorizontal: 20,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 34,
+    color: 'white',
+    marginLeft: 120,
+  },
+  button: {
+    marginLeft: 100,
+  },
+  innerContainer:{
+    flexGrow: 1, 
+    minHeight: '100%',
   }
 });
